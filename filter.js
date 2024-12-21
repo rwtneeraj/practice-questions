9abcfd2068b6cd33c09ee04e8a4b1f63a041af0c
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2024-12-21 20:21:08 +0530

    add filterEvenNumbers

--- filter.js
+++ filter.js
@@ -1,5 +1,17 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+};
+
 // even numbers [1, 2, 3, 4, 5] => [2, 4]
-const filterEvenNumbers = function (numbers) { };
+const filterEvenNumbers = function (numbers) {
+    return numbers.filter(function (num) {
+    if (isEven(num)) {
+      return num;
+    }
+  });
+ };
+
+console.log(filterEvenNumbers([1, 2, 3, 4, 5]));
 
 // words with more than 5 letters ["apple", "banana", "kiwi", "grape"] => ["banana"]
 const filterLongWords = function (words) { };
