acad072526e307587608fe8c5618b017cf0b31b9
rwtneeraj <183721769+rwtneeraj@users.noreply.github.com>
2024-12-21 21:38:43 +0530

    modified function in filterBySomething.js

--- filter/filterBySomething.js
+++ filter/filterBySomething.js
@@ -35,16 +35,46 @@ const filterBySalary = function (employees, salary) {
 console.log(filterBySalary([{ name: "Alice", salary: 5000 }, { name: "Bob", salary: 7000 }], 5000));
 
 // filter orders with a quantity greater than a given number [{orderId: 1, quantity: 10}, {orderId: 2, quantity: 5}] => [{orderId: 1, quantity: 10}]
-const filterByQuantity = function (orders, quantity) { };
+const filterByQuantity = function (orders, quantity) {
+  return orders.filter(function (order) {
+    return order.quantity > quantity;
+  });
+};
+
+console.log(filterByQuantity([{ orderId: 1, quantity: 10 }, { orderId: 2, quantity: 5 }], 5));
 
 // filter books published after a certain year [{title: "Book1", year: 2020}, {title: "Book2", year: 2022}] => [{title: "Book2", year: 2022}]
-const filterByYear = function (books, year) { };
+const filterByYear = function (books, year) {
+  return books.filter(function (book) {
+    return book.year > year;
+  });
+};
+
+console.log(filterByYear([{ title: "Book1", year: 2020 }, { title: "Book2", year: 2022 }], 2020));
 
 // filter students with a grade higher than a given threshold in a specific subject [{name: "Alice", grades: {math: 90, science: 80}}, {name: "Bob", grades: {math: 70, science: 85}}] => [{name: "Alice", grades: {math: 90, science: 80}}]
-const filterBySubjectGrade = function (students, subject, threshold) { };
+const filterBySubjectGrade = function (students, subject, threshold) {
+  return students.filter(function (student) {
+    return student.grades[subject] > threshold;
+  });
+};
+
+console.log(filterBySubjectGrade([{ name: "Alice", grades: { math: 90, science: 80 } }, { name: "Bob", grades: { math: 70, science: 85 } }], "science", 80));
 
 // filter photos with a minimum number of likes [{id: 1, likes: 100}, {id: 2, likes: 50}] => [{id: 1, likes: 100}]
-const filterByLikes = function (photos, likes) { };
+const filterByLikes = function (photos, likes) {
+  return photos.filter(function (photo) {
+    return photo.likes < likes;
+  });
+};
+
+console.log(filterByLikes([{ id: 1, likes: 100 }, { id: 2, likes: 50 }], 90));
 
 // filter users who have made a certain number of posts [{username: "Alice", posts: 10}, {username: "Bob", posts: 5}] => [{username: "Alice", posts: 10}]
-const filterByPostCount = function (users, postCount) { };
+const filterByPostCount = function (users, postCount) { 
+  return users.filter(function(user){
+    return user.posts === postCount;
+  })
+};
+
+console.log(filterByPostCount( [{username: "Alice", posts: 10}, {username: "Bob", posts: 5}],10))
